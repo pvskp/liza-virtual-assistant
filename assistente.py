3ed330e4b17000bbaa66390a6cdb5a8ff5c06203
pvskp <pvincius14@gmail.com>
2021-09-01 01:59:20 -0300

    Assistente já reconhece comando para se desligar

--- assistente.py
+++ assistente.py
@@ -1,25 +1,52 @@
 import pyttsx3
-import speech_recognition
-
+import speech_recognition as sr
+import time
 
 class Assistant():
     def __init__(self) -> None:
         self.text_to_speech = pyttsx3.init()        
-        self.text_to_speech.setProperty('voice', 'mb-br4')
-        self.text_to_speech.setProperty('rate', 100)
+        self.text_to_speech.setProperty('voice', 'mb-br1')
+        self.text_to_speech.setProperty('rate', 90)
+        self.recognizer = sr.Recognizer()
 
+    def listening(self):
+        command = "Comando não reconhecido"
+        with sr.Microphone() as source:
+            self.recognizer.pause_threshold = 1
+            audio = self.recognizer.listen(source)
+        try:
+            print("Ouvindo...")
+            command = self.recognizer.recognize_google(audio, language='pt-br')
+            print(command)
+
+        except Exception as listeningError:
+            print(listeningError)
+            self.speak("Desculpe, não entendi")
+
+        return command
 
     def speak(self, text):
         self.text_to_speech.say(text)
         self.text_to_speech.runAndWait()
 
     def falar_bom_dia(self):
-        self.text_to_speech.say("Bom dia, Paulo!")
+        self.text_to_speech.say("Bom dia Paulo")
+        time.sleep(0.4)
+        self.text_to_speech.say("Como estás")
+
         self.text_to_speech.runAndWait()
 
+def receber_comando(assistente):
+    while True:
+        comando = assistente.listening()
+        print(comando)
+        if (comando == "Desligar assistente"):
+            assistente.speak("Desligando assistente")
+            break
 
 def main():
     assistente = Assistant()
-    assistente.falar_bom_dia()
+    receber_comando(assistente)
 
-main()
+if __name__ == '__main__':
+    main()
