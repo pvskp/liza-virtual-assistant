3427f4432a6ae4f8f44f399a67020fccc0154f5d
pvskp <pvincius14@gmail.com>
2021-09-02 00:22:16 -0300

    Comenta iot functions

--- src/functionalities/iot_functions.py
+++ src/functionalities/iot_functions.py
@@ -1,13 +1,17 @@
 from os import system as execute
 
-def start_chiaki():
-    execute('Chiaki')
+# PS4 related
 
 def start_ps4():
     execute('ps4-waker')
 
 def standby_ps4():
     execute('ps4-waker standby')
+
+def start_chiaki():
+    execute('Chiaki')
+
+# Others
 
 def start_discord():
     execute('discord')
